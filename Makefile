07062b54c6c794760650bb9c079f12e28f62cec5
Paul Rowe <prowe@sourceallies.com>
2022-06-14 21:04:35 -0500

    Now able to run locally with real AWS creds.

--- Makefile
+++ Makefile
@@ -26,3 +26,10 @@ run-glue-container:
 		-w /work \
 		--entrypoint=bash \
 		amazon/aws-glue-libs:glue_libs_3.0.0_image_01
+
+save-aws-credentials:
+	aws configure set --profile=$(profile) aws_access_key_id $(AWS_ACCESS_KEY_ID)
+	aws configure set --profile=$(profile) aws_secret_access_key $(AWS_SECRET_ACCESS_KEY)
+	aws configure set --profile=$(profile) aws_session_token $(AWS_SESSION_TOKEN)
+	aws configure set --profile=$(profile) default.region us-east-1
+	@echo "to set the profile in the container: export AWS_DEFAULT_PROFILE=$(profile); export AWS_PROFILE=$(profile)"
--- src/simple_job/load_books.py
+++ src/simple_job/load_books.py
@@ -1,18 +1,26 @@
 from awsglue.context import GlueContext
-
-from pyspark.context import SparkContext
 from pyspark.sql import SparkSession, DataFrame
 
-def load_books(gc: GlueContext) -> DataFrame:
-    pass
+
+def load_books(glue_context: GlueContext) -> DataFrame:
+    # TODO: make this an arg
+    bucket_name = 'glue-reference-implementation-databucket-fed75mq4rmq0'
+    return glue_context.create_dynamic_frame_from_options(
+        connection_type="s3",
+        connection_options={
+            "paths": [f"s3://{bucket_name}/sample_data/json/books"]
+        },
+        format="json"
+    ).toDF()
 
 
 def save_books(books: DataFrame, gc: GlueContext):
-    pass 
-
-
-def main(gc: GlueContext):
     pass
+
+
+def main(glue_context: GlueContext):
+    books = load_books(glue_context)
+    books.show()
 
 
 if __name__ == '__main__':
