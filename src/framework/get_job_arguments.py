150e19041a9a067ef60ea7a4b470f6a9788c72ac
prowe <prowe@sourceallies.com>
2022-07-19 20:40:34 -0500

    adding a get_job_argument convienence func (#44)

--- src/framework/get_job_arguments.py
+++ src/framework/get_job_arguments.py
@@ -14,9 +14,17 @@ __auto_included_args = {
 
 def get_job_arguments(*arg_names: str) -> Tuple[str]:
     """
-    TODO: Docs here
+    Takes a variable array of argument names. Looks up these arguments and returns them in a tuple in that order
     """
     args_to_resolve = [arg for arg in arg_names if arg not in __auto_included_args]
     resolved_options = getResolvedOptions(sys.argv, args_to_resolve)
     print(resolved_options)
     return tuple(resolved_options[arg] for arg in arg_names)
+
+
+def get_job_argument(arg_name: str) -> str:
+    """
+    Convinence wrapper around get_job_arguments for getting a single argument
+    """
+    result, = get_job_arguments(arg_name)
+    return result
