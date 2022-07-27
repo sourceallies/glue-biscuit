48f516ea8e85074d98710c7a9e67b6054f86af21
Paul Rowe <prowe@sourceallies.com>
2022-07-26 21:35:41 -0500

    formatting

--- src/simple_job/test_load_books.py
+++ src/simple_job/test_load_books.py
@@ -94,7 +94,9 @@ def test_save_books(mock_glue_context: GlueContext):
         "glue_reference",
         "raw_books",
     )
-    call_order_mock.assert_has_calls([
-        call.purge_table(ANY, ANY, options=ANY),
-        call.write_dynamic_frame_from_catalog(ANY, ANY, ANY)
-    ])
+    call_order_mock.assert_has_calls(
+        [
+            call.purge_table(ANY, ANY, options=ANY),
+            call.write_dynamic_frame_from_catalog(ANY, ANY, ANY),
+        ]
+    )
