880316c9c0663febb0b5a675fd25b09bb6024bd0
prowe <prowe@sourceallies.com>
2022-07-28 20:56:53 -0500

    Create module "public" interface (#45)
    * setting up exports from the module so that external code does not need exact file
    
    * format

--- src/simple_job/test_load_books.py
+++ src/simple_job/test_load_books.py
@@ -4,9 +4,12 @@ from awsglue.context import GlueContext
 from pyspark.sql import DataFrame
 from awsglue.context import GlueContext
 from unittest.mock import patch, Mock, call, ANY
-from framework.data_frame_matcher import DataFrameMatcher
-from framework.dynamic_frame_matcher import DynamicFrameMatcher
-from framework.fixtures import spark_context, mock_glue_context
+from framework import (
+    DataFrameMatcher,
+    DynamicFrameMatcher,
+    spark_context,
+    mock_glue_context,
+)
 from simple_job.load_books import main, load_books, save_books
 
 
@@ -94,7 +97,9 @@ def test_save_books(mock_glue_context: GlueContext):
         "glue_reference",
         "raw_books",
     )
-    call_order_mock.assert_has_calls([
-        call.purge_table(ANY, ANY, options=ANY),
-        call.write_dynamic_frame_from_catalog(ANY, ANY, ANY)
-    ])
+    call_order_mock.assert_has_calls(
+        [
+            call.purge_table(ANY, ANY, options=ANY),
+            call.write_dynamic_frame_from_catalog(ANY, ANY, ANY),
+        ]
+    )
