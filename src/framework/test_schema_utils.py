7a00cfd8236b01f673acd9962825c6181a4ee1fd
Gene <tinderholmgene@gmail.com>
2022-08-25 19:52:29 -0500

    add tests for schema utils

--- src/framework/test_schema_utils.py
+++ src/framework/test_schema_utils.py
@@ -1,6 +1,6 @@
 import pytest
 
-from framework.schema_utils import coerce_to_schema
+from framework.schema_utils import coerce_to_schema, schema_from_cloudformation
 from framework import DataFrameMatcher
 from pyspark.sql import DataFrame, Row, SparkSession
 from pyspark.sql.types import (
@@ -10,6 +10,11 @@ from pyspark.sql.types import (
     LongType,
     ArrayType,
     StringType,
+    ByteType,
+    ShortType,
+    DateType,
+    TimestampType,
+    DecimalType,
 )
 
 
@@ -21,7 +26,8 @@ def test_df(spark_session: SparkSession) -> DataFrame:
 
 
 def test_coerces_single_column(test_df: DataFrame):
-    result = coerce_to_schema(test_df, StructType([StructField("x", LongType())]))
+    result = coerce_to_schema(
+        test_df, StructType([StructField("x", LongType())]))
 
     assert isinstance(result.schema["x"].dataType, LongType)
     assert result == DataFrameMatcher([{"x": 1}, {"x": 2}])
@@ -29,7 +35,8 @@ def test_coerces_single_column(test_df: DataFrame):
 
 def test_removes_extra_columns(test_df: DataFrame):
     test_df = test_df.withColumn("y", test_df["x"])
-    result = coerce_to_schema(test_df, StructType([StructField("y", LongType())]))
+    result = coerce_to_schema(
+        test_df, StructType([StructField("y", LongType())]))
 
     assert result.schema.fieldNames() == ["y"]
     assert result == DataFrameMatcher([{"y": 1}, {"y": 2}])
@@ -46,7 +53,8 @@ def test_coerces_struct_types(spark_session: SparkSession):
                         [
                             StructField(
                                 "nested_struct_field",
-                                StructType([StructField("value_field", IntegerType())]),
+                                StructType(
+                                    [StructField("value_field", IntegerType())]),
                             )
                         ]
                     ),
@@ -65,7 +73,8 @@ def test_coerces_struct_types(spark_session: SparkSession):
                         [
                             StructField(
                                 "nested_struct_field",
-                                StructType([StructField("value_field", LongType())]),
+                                StructType(
+                                    [StructField("value_field", LongType())]),
                             )
                         ]
                     ),
@@ -90,7 +99,8 @@ def test_coerces_array_types(spark_session: SparkSession):
         [{"x": [1, 2, 3, 4]}],
         schema=StructType([StructField("x", ArrayType(IntegerType()))]),
     )
-    result = coerce_to_schema(df, StructType([StructField("x", ArrayType(LongType()))]))
+    result = coerce_to_schema(df, StructType(
+        [StructField("x", ArrayType(LongType()))]))
 
     array_field: ArrayType = result.schema["x"].dataType
     assert isinstance(array_field.elementType, LongType)
@@ -98,7 +108,8 @@ def test_coerces_array_types(spark_session: SparkSession):
 
 
 def test_coerces_binary_incompatible_types(test_df: DataFrame):
-    result = coerce_to_schema(test_df, StructType([StructField("x", StringType())]))
+    result = coerce_to_schema(test_df, StructType(
+        [StructField("x", StringType())]))
 
     assert isinstance(result.schema["x"].dataType, StringType)
     assert result == DataFrameMatcher(
@@ -107,3 +118,110 @@ def test_coerces_binary_incompatible_types(test_df: DataFrame):
             {"x": "2"},
         ]
     )
+
+
+def test_schema_from_cloudformation_parses_string():
+    schema = schema_from_cloudformation("./test_template.yml", "test_table")
+    fields = schema.fields
+    field_under_test: StructField = [
+        field for field in fields if field.name == "title"][0]
+    assert isinstance(field_under_test.dataType, StringType)
+
+
+def test_schema_from_cloudformation_parses_tinyint():
+    schema = schema_from_cloudformation("./test_template.yml", "test_table")
+    fields = schema.fields
+    field_under_test: StructField = [
+        field for field in fields if field.name == "author_age"][0]
+    assert isinstance(field_under_test.dataType, ByteType)
+
+
+def test_schema_from_cloudformation_parses_smallint():
+    schema = schema_from_cloudformation("./test_template.yml", "test_table")
+    fields = schema.fields
+    field_under_test: StructField = [
+        field for field in fields if field.name == "puddles"][0]
+    assert isinstance(field_under_test.dataType, ShortType)
+
+
+def test_schema_from_cloudformation_parses_int():
+    schema = schema_from_cloudformation("./test_template.yml", "test_table")
+    fields = schema.fields
+    field_under_test: StructField = [
+        field for field in fields if field.name == "kangaroos_in_australia"][0]
+    assert isinstance(field_under_test.dataType, IntegerType)
+
+
+def test_schema_from_cloudformation_parses_long():
+    schema = schema_from_cloudformation("./test_template.yml", "test_table")
+    fields = schema.fields
+    field_under_test: StructField = [
+        field for field in fields if field.name == "trees_in_world"][0]
+    assert isinstance(field_under_test.dataType, LongType)
+
+
+def test_schema_from_cloudformation_parses_date():
+    schema = schema_from_cloudformation("./test_template.yml", "test_table")
+    fields = schema.fields
+    field_under_test: StructField = [
+        field for field in fields if field.name == "publish_date"][0]
+    assert isinstance(field_under_test.dataType, DateType)
+
+
+def test_schema_from_cloudformation_parses_timestamp():
+    schema = schema_from_cloudformation("./test_template.yml", "test_table")
+    fields = schema.fields
+    field_under_test: StructField = [
+        field for field in fields if field.name == "author_time"][0]
+    assert isinstance(field_under_test.dataType, TimestampType)
+
+
+def test_schema_from_cloudformation_parses_decimal():
+    schema = schema_from_cloudformation("./test_template.yml", "test_table")
+    fields = schema.fields
+    field_under_test: StructField = [
+        field for field in fields if field.name == "royalties_owed"][0]
+    data_type: DecimalType = field_under_test.dataType
+    assert isinstance(data_type, DecimalType)
+    assert data_type.precision == 38
+    assert data_type.scale == 2
+
+
+def test_schema_from_cloudformation_parses_struct():
+    schema = schema_from_cloudformation("./test_template.yml", "test_table")
+    fields = schema.fields
+    field_under_test: StructField = [
+        field for field in fields if field.name == "pet"][0]
+    data_type: StructType = field_under_test.dataType
+    assert isinstance(data_type, StructType)
+    assert data_type.fields == [
+        StructField("name", StringType()),
+        StructField("age", IntegerType()),
+    ]
+
+
+def test_schema_from_cloudformation_parses_array():
+    schema = schema_from_cloudformation("./test_template.yml", "test_table")
+    fields = schema.fields
+    field_under_test: StructField = [
+        field for field in fields if field.name == "stats"][0]
+    data_type: ArrayType = field_under_test.dataType
+    assert isinstance(data_type, ArrayType)
+    assert isinstance(data_type.elementType, IntegerType)
+
+
+def test_schema_from_cloudformation_parses_array_of_structs():
+    schema = schema_from_cloudformation("./test_template.yml", "test_table")
+    fields = schema.fields
+    field_under_test: StructField = [
+        field for field in fields if field.name == "metadata"][0]
+
+    data_type: ArrayType = field_under_test.dataType
+    internal_struct: StructType = data_type.elementType
+
+    assert isinstance(data_type, ArrayType)
+    assert isinstance(internal_struct, StructType)
+    assert internal_struct.fields == [
+        StructField("key", StringType()),
+        StructField("value", StringType()),
+    ]
