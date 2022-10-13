c6c9b6c7e4d8936c44523b880d1c47b375c8b908
Gene Tinderholm <tinderholmgene@gmail.com>
2022-10-13 08:12:17 -0500

    add new annotations for sourcing data (#57)
    * add new annotations for sourcing data
    
    * run black over this

--- src/framework/schema_utils.py
+++ src/framework/schema_utils.py
@@ -1,5 +1,9 @@
-from pyspark.sql import DataFrame
+from awsglue import DynamicFrame
+from awsglue.context import GlueContext
+from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.types import StructType
+from functools import wraps
+from typing import Callable
 
 
 def coerce_to_schema(df: DataFrame, schema: StructType):
@@ -10,3 +14,75 @@ def coerce_to_schema(df: DataFrame, schema: StructType):
         result = result.withColumn(field.name, result[field.name].cast(field.dataType))
 
     return result
+
+
+def __coalesce(*args):
+    for arg in args:
+        if arg is not None:
+            return arg
+
+
+def __get_schema_args(*args):
+    first_non_none = __coalesce(*args)
+    if callable(first_non_none):
+        return first_non_none()
+    return first_non_none
+
+
+def __force_frame_to_fit(schema, *args, **kwargs):
+    new_args = [*args]
+    for i in range(len(args)):
+        if isinstance(new_args[i], DynamicFrame):
+            new_args[i] = new_args[i].toDF()
+        if isinstance(new_args[i], DataFrame):
+            new_args[i] = coerce_to_schema(new_args[i], schema)
+
+    new_kwargs = {**kwargs}
+    for k, _ in new_kwargs.items():
+        if isinstance(new_kwargs[k], DynamicFrame):
+            new_kwargs[k] = new_kwargs[k].toDF()
+        if isinstance(new_kwargs[k], DataFrame):
+            new_kwargs[k] = coerce_to_schema(new_kwargs[k], schema)
+    return new_args, new_kwargs
+
+
+def schema(*, schema_obj=None, schema_func=None):
+    """
+    Takes a pyspark schema or a function resulting in a pyspark schema and coerces the input frame to match this schema.
+    """
+
+    def annotation_func(func):
+        @wraps(func)
+        def wrapper_func(*args, **kwargs):
+            final_schema = __get_schema_args(schema_obj, schema_func)
+            new_args, new_kwargs = __force_frame_to_fit(final_schema, *args, **kwargs)
+            return func(*new_args, **new_kwargs)
+
+        return wrapper_func
+
+    return annotation_func
+
+
+def source(database: str, table: str, schema_obj=None, schema_func=None):
+    spark = SparkSession.builder.getOrCreate()
+    glue_context = GlueContext(spark.sparkContext)
+
+    def annotation_func(func: Callable):
+        @wraps(func)
+        def wrapper_func(*args, **kwargs):
+            final_schema = __get_schema_args(schema_obj, schema_func)
+            dyn_frame = glue_context.create_dynamic_frame_from_catalog(
+                database=database,
+                table_name=table,
+                transformation_ctx=f"source-{database}-{table}",
+            )
+            df = dyn_frame.toDF()
+            fitted_frame = coerce_to_schema(df, final_schema)
+            # Do we want to assume this is right here,
+            # or do we want to do crazy reflection magic
+            # to figure out a keyword arg? Both?
+            return func(fitted_frame, *kwargs, **kwargs)
+
+        return wrapper_func
+
+    return annotation_func
--- src/framework/test_schema_utils.py
+++ src/framework/test_schema_utils.py
@@ -1,7 +1,10 @@
 import pytest
-
-from framework.schema_utils import coerce_to_schema
+from unittest.mock import patch, Mock
+from awsglue import DynamicFrame
+from awsglue.context import GlueContext
+from framework.schema_utils import coerce_to_schema, source, schema
 from framework.test import DataFrameMatcher
+from pyspark import SparkContext
 from pyspark.sql import DataFrame, Row, SparkSession
 from pyspark.sql.types import (
     StructType,
@@ -18,6 +21,11 @@ def test_df(spark_session: SparkSession) -> DataFrame:
     yield spark_session.createDataFrame(
         [{"x": 1}, {"x": 2}], StructType([StructField("x", IntegerType())])
     )
+
+
+@pytest.fixture
+def test_dyf(test_df: DataFrame, spark_context: SparkContext) -> DataFrame:
+    return DynamicFrame.fromDF(test_df, GlueContext(spark_context), "test_dyf")
 
 
 def test_coerces_single_column(test_df: DataFrame):
@@ -107,3 +115,55 @@ def test_coerces_binary_incompatible_types(test_df: DataFrame):
             {"x": "2"},
         ]
     )
+
+
+@schema(schema_obj=StructType([StructField("x", LongType())]))
+def test_schema_coerces_data_frame_from_schema_object(test_df):
+    assert isinstance(test_df.schema["x"].dataType, LongType)
+    assert test_df == DataFrameMatcher([{"x": 1}, {"x": 2}])
+
+
+@schema(schema_func=lambda: StructType([StructField("x", LongType())]))
+def test_schema_coerces_data_frame_from_schema_function(test_df):
+    assert isinstance(test_df.schema["x"].dataType, LongType)
+    assert test_df == DataFrameMatcher([{"x": 1}, {"x": 2}])
+
+
+@patch("framework.schema_utils.GlueContext")
+def test_source_gets_table_from_glue(mock_glue_context_class: Mock):
+    mock_glue_context = Mock(name="GlueContext")
+    mock_glue_context_class.return_value = mock_glue_context
+
+    @source(
+        "some_db", "some_table", schema_obj=StructType([StructField("x", LongType())])
+    )
+    def test_func(frame):
+        pass
+
+    test_func()
+
+    mock_glue_context.assert_called_once_with(
+        database="some_db",
+        table_name="some_table",
+        transformation_ctx="source-some_db-some_table",
+    )
+
+
+@patch("framework.schema_utils.GlueContext")
+def test_source_gets_table_from_glue(
+    mock_glue_context_class: Mock, test_dyf: DynamicFrame
+):
+    mock_glue_context: DynamicFrame = Mock(name="GlueContext")
+    mock_glue_context.create_dynamic_frame_from_catalog.return_value = test_dyf
+    mock_glue_context_class.return_value = mock_glue_context
+
+    @source(
+        "some_db", "some_table", schema_obj=StructType([StructField("x", LongType())])
+    )
+    def test_func(frame):
+        return frame
+
+    result = test_func()
+
+    assert isinstance(result.schema["x"].dataType, LongType)
+    assert result == DataFrameMatcher([{"x": 1}, {"x": 2}])
