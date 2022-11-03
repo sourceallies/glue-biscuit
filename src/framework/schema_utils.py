53279d0b8dc8d1695f7115ff1e594d8c1b297e88
Gene Tinderholm <tinderholmgene@gmail.com>
2022-11-03 08:26:49 -0500

    Schema retrieval and sink (#54)
    * add cfn-flip
    
    * add tests for schema utils
    
    * add schema utils
    
    * run format over the thing
    
    * move test_template into its own directory
    
    * re-work dockerfile to account for path issues
    
    * format
    
    * fix tests in pipeline
    
    * add get_schema_from_glue
    
    * add sink annotation
    
    * format
    
    * revert unused dockerfile

--- src/framework/schema_utils.py
+++ src/framework/schema_utils.py
@@ -1,7 +1,24 @@
+import os
+import re
+import boto3
+from typing import List
+from cfn_tools import load_yaml
 from awsglue import DynamicFrame
 from awsglue.context import GlueContext
 from pyspark.sql import SparkSession, DataFrame
-from pyspark.sql.types import StructType
+from pyspark.sql.types import (
+    StructType,
+    StructField,
+    StringType,
+    ShortType,
+    ByteType,
+    IntegerType,
+    LongType,
+    DateType,
+    TimestampType,
+    DecimalType,
+    ArrayType,
+)
 from functools import wraps
 from typing import Callable
 
@@ -14,6 +31,90 @@ def coerce_to_schema(df: DataFrame, schema: StructType):
         result = result.withColumn(field.name, result[field.name].cast(field.dataType))
 
     return result
+
+
+def __get_glue_table(resources: dict, table_name: str):
+    for _logical_id, resource_definition in resources.items():
+        try:
+            name = resource_definition["Properties"]["TableInput"]["Name"]
+            if resource_definition["Type"] == "AWS::Glue::Table" and name == table_name:
+                return resource_definition
+        except KeyError:
+            pass
+
+
+class BadTypeException(Exception):
+    pass
+
+
+def __handle_decimal(match: re.Match):
+    (precision, scale) = match.groups()
+    return DecimalType(int(precision), int(scale))
+
+
+def __handle_hive_struct_field(struct_field: str):
+    [name, field_type] = struct_field.split(":")
+    return StructField(name, __convert_type(field_type))
+
+
+def __handle_hive_struct(hive_struct_types: str):
+    return [__handle_hive_struct_field(field) for field in hive_struct_types.split(",")]
+
+
+def __handle_struct(match: re.Match):
+    (struct_fields,) = match.groups()
+    return StructType(__handle_hive_struct(struct_fields))
+
+
+def __handle_array(match: re.Match):
+    (array_type,) = match.groups()
+    return ArrayType(__convert_type(array_type))
+
+
+def __convert_type(column_type: str):
+    options = [
+        (lambda t: t == "string", lambda _match: StringType()),
+        (lambda t: t == "tinyint", lambda _match: ByteType()),
+        (lambda t: t == "smallint", lambda _match: ShortType()),
+        (lambda t: t == "int", lambda _match: IntegerType()),
+        (lambda t: t == "bigint", lambda _match: LongType()),
+        (lambda t: t == "date", lambda _match: DateType()),
+        (lambda t: t == "timestamp", lambda _match: TimestampType()),
+        (lambda t: re.match(r"decimal\((\d+),(\d+)\)", t), __handle_decimal),
+        (lambda t: re.match(r"^struct<(.*)>$", t), __handle_struct),
+        (lambda t: re.match(r"^array<(.*)>$", t), __handle_array),
+    ]
+    for test_func, type_func in options:
+        match = test_func(column_type.lower())
+        if match:
+            return type_func(match)
+    raise BadTypeException(f"could not find type for {column_type}")
+
+
+def __convert_column(column):
+    return StructField(column["Name"], __convert_type(column["Type"]))
+
+
+def __convert_columns(columns: List[dict]):
+    return [__convert_column(column) for column in columns]
+
+
+def schema_from_cloudformation(path_to_template: str, table_name: str) -> StructType:
+    template_text = open(path_to_template, "r").read()
+    parsed_template = load_yaml(template_text)
+    resources = parsed_template["Resources"]
+    table = __get_glue_table(resources, table_name)
+    return StructType(
+        __convert_columns(
+            table["Properties"]["TableInput"]["StorageDescriptor"]["Columns"]
+        )
+    )
+
+
+def schema_from_glue(database_name: str, table_name: str):
+    glue = boto3.client("glue", os.environ["AWS_REGION"])
+    res = glue.get_table(DatabaseName=database_name, Name=table_name)
+    return StructType(__convert_columns(res["Table"]["StorageDescriptor"]["Columns"]))
 
 
 def __coalesce(*args):
@@ -82,6 +183,27 @@ def source(database: str, table: str, schema_obj=None, schema_func=None):
             # or do we want to do crazy reflection magic
             # to figure out a keyword arg? Both?
             return func(fitted_frame, *kwargs, **kwargs)
+
+        return wrapper_func
+
+    return annotation_func
+
+
+def sink(database: str, table: str, schema_obj=None, schema_func=None):
+    spark = SparkSession.builder.getOrCreate()
+    glue_context = GlueContext(spark.sparkContext)
+
+    def annotation_func(func: Callable):
+        @wraps(func)
+        def wrapper_func(*args, **kwargs):
+            df = func(*args, **kwargs)
+            final_schema = __get_schema_args(schema_obj, schema_func)
+            fitted_frame = coerce_to_schema(df, final_schema)
+            dyf = DynamicFrame.fromDF(
+                fitted_frame, glue_context, f"sink-{database}-{table}"
+            )
+            glue_context.write_dynamic_frame_from_catalog(dyf, database, table)
+            return fitted_frame
 
         return wrapper_func
 
--- src/framework/test_schema_utils.py
+++ src/framework/test_schema_utils.py
@@ -1,9 +1,15 @@
+import os
 import pytest
+from framework.schema_utils import (
+    coerce_to_schema,
+    schema_from_cloudformation,
+    schema_from_glue,
+)
 from unittest.mock import patch, Mock
 from awsglue import DynamicFrame
 from awsglue.context import GlueContext
-from framework.schema_utils import coerce_to_schema, source, schema
-from framework.test import DataFrameMatcher
+from framework.schema_utils import coerce_to_schema, source, schema, sink
+from framework.test import DataFrameMatcher, DynamicFrameMatcher
 from pyspark import SparkContext
 from pyspark.sql import DataFrame, Row, SparkSession
 from pyspark.sql.types import (
@@ -13,7 +19,14 @@ from pyspark.sql.types import (
     LongType,
     ArrayType,
     StringType,
+    ByteType,
+    ShortType,
+    DateType,
+    TimestampType,
+    DecimalType,
 )
+
+test_template_path = "./test_data/test_template.yml"
 
 
 @pytest.fixture
@@ -26,6 +39,105 @@ def test_df(spark_session: SparkSession) -> DataFrame:
 @pytest.fixture
 def test_dyf(test_df: DataFrame, spark_context: SparkContext) -> DataFrame:
     return DynamicFrame.fromDF(test_df, GlueContext(spark_context), "test_dyf")
+
+
+@pytest.fixture(autouse=True)
+def mock_environ():
+    old_region = os.environ.get("AWS_REGION", "")
+    os.environ["AWS_REGION"] = "us-north-5"
+    yield
+    os.environ["AWS_REGION"] = old_region
+
+
+@pytest.fixture
+def mock_glue_context(test_dyf: DynamicFrame, spark_context: SparkContext):
+    real_context = GlueContext(spark_context)
+    mock_context = Mock(name="GlueContext")
+    mock_context.create_dynamic_frame_from_catalog.return_value = test_dyf
+    mock_context._jvm = real_context._jvm
+    mock_context._ssql_ctx = real_context._ssql_ctx
+    return mock_context
+
+
+@pytest.fixture(autouse=True)
+def mock_glue_context_class(mock_glue_context):
+    with patch("framework.schema_utils.GlueContext") as mock_class:
+        mock_class.return_value = mock_glue_context
+        yield mock_class
+
+
+@pytest.fixture
+def glue_response():
+    return {
+        "Table": {
+            "Name": "books",
+            "DatabaseName": "glue_reference",
+            "Description": "Books data product",
+            "CreateTime": "2022-08-11T21:55:34-05:00",
+            "UpdateTime": "2022-08-21T20:05:54-05:00",
+            "Retention": 0,
+            "StorageDescriptor": {
+                "Columns": [
+                    {
+                        "Name": "title",
+                        "Type": "string",
+                        "Comment": "Full title of this book",
+                    },
+                    {
+                        "Name": "publish_date",
+                        "Type": "date",
+                        "Comment": "The date this book was first published",
+                    },
+                    {
+                        "Name": "author_name",
+                        "Type": "string",
+                        "Comment": "The full name of the author",
+                    },
+                    {
+                        "Name": "author_birth_date",
+                        "Type": "date",
+                        "Comment": "The date the author was born",
+                    },
+                    {
+                        "Name": "author_id",
+                        "Type": "bigint",
+                        "Comment": "Unique identifier for this author",
+                    },
+                ],
+                "Location": "s3://glue-reference-implementation-databucket-fed75mq4rmq0/books",
+                "InputFormat": "org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat",
+                "OutputFormat": "org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat",
+                "Compressed": False,
+                "NumberOfBuckets": 0,
+                "SerdeInfo": {
+                    "SerializationLibrary": "org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe"
+                },
+                "SortColumns": [],
+                "Parameters": {"classification": "parquet"},
+                "StoredAsSubDirectories": False,
+            },
+            "CreatedBy": "arn:aws:sts::144406111952:assumed-role/CognitoSAI-FederatedUserRole-3IHKNHJY8G3F/CognitoIdentityCredentials",
+            "IsRegisteredWithLakeFormation": False,
+            "CatalogId": "144406111952",
+            "VersionId": "2",
+        }
+    }
+
+
+@pytest.fixture
+def mock_glue_client(glue_response):
+    glue = Mock(name="glue_client")
+    glue.get_table.return_value = glue_response
+    yield glue
+
+
+@pytest.fixture(autouse=True)
+def mock_boto3(mock_glue_client):
+    with patch("framework.schema_utils.boto3") as boto:
+        boto.client.side_effect = lambda *args, **kwargs: {"glue": mock_glue_client}[
+            args[0]
+        ]
+        yield boto
 
 
 def test_coerces_single_column(test_df: DataFrame):
@@ -117,6 +229,137 @@ def test_coerces_binary_incompatible_types(test_df: DataFrame):
     )
 
 
+def test_schema_from_cloudformation_parses_string():
+    schema = schema_from_cloudformation(test_template_path, "test_table")
+    fields = schema.fields
+    field_under_test: StructField = [
+        field for field in fields if field.name == "title"
+    ][0]
+    assert isinstance(field_under_test.dataType, StringType)
+
+
+def test_schema_from_cloudformation_parses_tinyint():
+    schema = schema_from_cloudformation(test_template_path, "test_table")
+    fields = schema.fields
+    field_under_test: StructField = [
+        field for field in fields if field.name == "author_age"
+    ][0]
+    assert isinstance(field_under_test.dataType, ByteType)
+
+
+def test_schema_from_cloudformation_parses_smallint():
+    schema = schema_from_cloudformation(test_template_path, "test_table")
+    fields = schema.fields
+    field_under_test: StructField = [
+        field for field in fields if field.name == "puddles"
+    ][0]
+    assert isinstance(field_under_test.dataType, ShortType)
+
+
+def test_schema_from_cloudformation_parses_int():
+    schema = schema_from_cloudformation(test_template_path, "test_table")
+    fields = schema.fields
+    field_under_test: StructField = [
+        field for field in fields if field.name == "kangaroos_in_australia"
+    ][0]
+    assert isinstance(field_under_test.dataType, IntegerType)
+
+
+def test_schema_from_cloudformation_parses_long():
+    schema = schema_from_cloudformation(test_template_path, "test_table")
+    fields = schema.fields
+    field_under_test: StructField = [
+        field for field in fields if field.name == "trees_in_world"
+    ][0]
+    assert isinstance(field_under_test.dataType, LongType)
+
+
+def test_schema_from_cloudformation_parses_date():
+    schema = schema_from_cloudformation(test_template_path, "test_table")
+    fields = schema.fields
+    field_under_test: StructField = [
+        field for field in fields if field.name == "publish_date"
+    ][0]
+    assert isinstance(field_under_test.dataType, DateType)
+
+
+def test_schema_from_cloudformation_parses_timestamp():
+    schema = schema_from_cloudformation(test_template_path, "test_table")
+    fields = schema.fields
+    field_under_test: StructField = [
+        field for field in fields if field.name == "author_time"
+    ][0]
+    assert isinstance(field_under_test.dataType, TimestampType)
+
+
+def test_schema_from_cloudformation_parses_decimal():
+    schema = schema_from_cloudformation(test_template_path, "test_table")
+    fields = schema.fields
+    field_under_test: StructField = [
+        field for field in fields if field.name == "royalties_owed"
+    ][0]
+    data_type: DecimalType = field_under_test.dataType
+    assert isinstance(data_type, DecimalType)
+    assert data_type.precision == 38
+    assert data_type.scale == 2
+
+
+def test_schema_from_cloudformation_parses_struct():
+    schema = schema_from_cloudformation(test_template_path, "test_table")
+    fields = schema.fields
+    field_under_test: StructField = [field for field in fields if field.name == "pet"][
+        0
+    ]
+    data_type: StructType = field_under_test.dataType
+    assert isinstance(data_type, StructType)
+    assert data_type.fields == [
+        StructField("name", StringType()),
+        StructField("age", IntegerType()),
+    ]
+
+
+def test_schema_from_cloudformation_parses_array():
+    schema = schema_from_cloudformation(test_template_path, "test_table")
+    fields = schema.fields
+    field_under_test: StructField = [
+        field for field in fields if field.name == "stats"
+    ][0]
+    data_type: ArrayType = field_under_test.dataType
+    assert isinstance(data_type, ArrayType)
+    assert isinstance(data_type.elementType, IntegerType)
+
+
+def test_schema_from_cloudformation_parses_array_of_structs():
+    schema = schema_from_cloudformation(test_template_path, "test_table")
+    fields = schema.fields
+    field_under_test: StructField = [
+        field for field in fields if field.name == "metadata"
+    ][0]
+
+    data_type: ArrayType = field_under_test.dataType
+    internal_struct: StructType = data_type.elementType
+
+    assert isinstance(data_type, ArrayType)
+    assert isinstance(internal_struct, StructType)
+    assert internal_struct.fields == [
+        StructField("key", StringType()),
+        StructField("value", StringType()),
+    ]
+
+
+def test_schema_from_glue_parses_glue_response():
+    res = schema_from_glue("some_db", "some_table")
+    assert res == StructType(
+        [
+            StructField("title", StringType()),
+            StructField("publish_date", DateType()),
+            StructField("author_name", StringType()),
+            StructField("author_birth_date", DateType()),
+            StructField("author_id", LongType()),
+        ]
+    )
+
+
 @schema(schema_obj=StructType([StructField("x", LongType())]))
 def test_schema_coerces_data_frame_from_schema_object(test_df):
     assert isinstance(test_df.schema["x"].dataType, LongType)
@@ -129,11 +372,7 @@ def test_schema_coerces_data_frame_from_schema_function(test_df):
     assert test_df == DataFrameMatcher([{"x": 1}, {"x": 2}])
 
 
-@patch("framework.schema_utils.GlueContext")
-def test_source_gets_table_from_glue(mock_glue_context_class: Mock):
-    mock_glue_context = Mock(name="GlueContext")
-    mock_glue_context_class.return_value = mock_glue_context
-
+def test_source_gets_table_from_glue(mock_glue_context: Mock):
     @source(
         "some_db", "some_table", schema_obj=StructType([StructField("x", LongType())])
     )
@@ -142,21 +381,14 @@ def test_source_gets_table_from_glue(mock_glue_context_class: Mock):
 
     test_func()
 
-    mock_glue_context.assert_called_once_with(
+    mock_glue_context.create_dynamic_frame_from_catalog.assert_called_once_with(
         database="some_db",
         table_name="some_table",
         transformation_ctx="source-some_db-some_table",
     )
 
 
-@patch("framework.schema_utils.GlueContext")
-def test_source_gets_table_from_glue(
-    mock_glue_context_class: Mock, test_dyf: DynamicFrame
-):
-    mock_glue_context: DynamicFrame = Mock(name="GlueContext")
-    mock_glue_context.create_dynamic_frame_from_catalog.return_value = test_dyf
-    mock_glue_context_class.return_value = mock_glue_context
-
+def test_source_coerces_schema():
     @source(
         "some_db", "some_table", schema_obj=StructType([StructField("x", LongType())])
     )
@@ -167,3 +399,31 @@ def test_source_gets_table_from_glue(
 
     assert isinstance(result.schema["x"].dataType, LongType)
     assert result == DataFrameMatcher([{"x": 1}, {"x": 2}])
+
+
+def test_sink_writes_table_to_glue(mock_glue_context: Mock, test_dyf: DynamicFrame):
+    @sink(
+        "some_db", "some_table", schema_obj=StructType([StructField("x", LongType())])
+    )
+    def test_func():
+        return test_dyf.toDF()
+
+    test_func()
+
+    mock_glue_context.write_dynamic_frame_from_catalog.assert_called_once_with(
+        DynamicFrameMatcher([{"x": 1}, {"x": 2}]), "some_db", "some_table"
+    )
+
+
+def test_sink_coerces_schema(mock_glue_context: Mock, test_dyf: DynamicFrame):
+    @sink(
+        "some_db", "some_table", schema_obj=StructType([StructField("x", StringType())])
+    )
+    def test_func():
+        return test_dyf.toDF()
+
+    test_func()
+
+    mock_glue_context.write_dynamic_frame_from_catalog.assert_called_once_with(
+        DynamicFrameMatcher([{"x": "1"}, {"x": "2"}]), "some_db", "some_table"
+    )
